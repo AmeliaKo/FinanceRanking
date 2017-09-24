9ac517815ded29e71d428cc15502b24861510f99
Myeongbu <kmb@smtown.com>
2017-09-24 15:23:19 +0900

    [Myeongbu] 금융 랭킹 앱 프로젝트 최초 커밋 및 테스트 코드 작성

---
+++ FinanceRanking/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FinanceRanking
+//
+//  Created by UramMyeongbu on 2017. 9. 23..
+//  Copyright © 2017년 UramMyeongbu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
